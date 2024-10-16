24ec264c820e6d20ee6f1926432c9f470e9283ce
newNotebook <machy179@gmail.com>
2024-10-16 13:16:39 +0200

    Odstranění chyby Firebase Analytics

--- app/src/main/java/com/machy1979ii/intervaltimer/services/TimerAnalytics.kt
+++ app/src/main/java/com/machy1979ii/intervaltimer/services/TimerAnalytics.kt
@@ -3,6 +3,7 @@ package com.machy1979ii.intervaltimer.services
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.analytics.FirebaseAnalytics
 
 object TimerAnalytics {
@@ -12,12 +13,15 @@ object TimerAnalytics {
         try {
             if (mFirebaseAnalytics == null) {
                 mFirebaseAnalytics = FirebaseAnalytics.getInstance(context)
+                Log.d("IntervalT Analytics:", "Inicializace analytiky 1")
             }
+            Log.d("IntervalT Analytics:", "Inicializace analytiky 2")
         } catch (e: Exception) {
             // Zalogujte chybu
-            Log.e("Boxing Analytics:", "Error initializing Firebase Analytics: ${e.message}")
+            Log.e("IntervalT Analytics:", "Error initializing Firebase Analytics: ${e.message}")
             // Oznamujte uživateli (volitelně)
-            // Toast.makeText(context, "Došlo k chybě při inicializaci analytiky.", Toast.LENGTH_SHORT).show()
+
+            Log.d("IntervalT Analytics:", "Došlo k chybě při inicializaci analytiky.")
         }
 /*        synchronized(this) {
             if (mFirebaseAnalytics == null) {
@@ -31,7 +35,8 @@ object TimerAnalytics {
         val bundle = Bundle().apply {
             putString(FirebaseAnalytics.Param.ITEM_NAME, activityName)
         }
-        Log.d("Boxing Analytics:", "1:"+activityName)
+        Log.d("IntervalT Analytics:", "1:"+activityName)
         mFirebaseAnalytics?.logEvent(activityName+"_opened", bundle)
+        Log.d("IntervalT Analytics:", "2:"+activityName)
     }
 }
